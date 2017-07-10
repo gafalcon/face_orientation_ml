759d316373b447b9365a19a497aec201e97dee65
Gabriel Falcones <gafalcon@espol.edu.ec>
2017-07-10 17:01:52 -0500

    Create big csv training data file

--- data_merging.py
+++ data_merging.py
@@ -21,3 +21,10 @@ down_training = create_training_datasets(down, [up, left, right, center], "down_
 left_training = create_training_datasets(left, [down, up, right, center], "left_training.csv")
 right_training = create_training_datasets(right, [down, left, up, center], "right_training.csv")
 center_training = create_training_datasets(center, [down, left, right, up], "center_training.csv")
+
+
+csv_files = ["up3", "up4", "down3", "down4", "left3", "left4", "right3_1", "right3_2", "right4", "center3", "center4"]
+
+csvs = [pandas.read_csv(path+csv_file+"_res.csv") for csv_file in csv_files]
+training_df = pandas.concat(csvs)
+training_df.to_csv(path+"training_dataset.csv")
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-
-"""
-
 import pandas
-up = pandas.read_csv("up.csv");
 
-#TODO validate for missing values
 def distance(row):
     if (row == 0).any():
         return -1
     return ((row[0]-row[2])**2 + (row[1]-row[3])**2)/(row[4])
 
-def clean_data(filename):
-    df = pandas.read_csv(filename+".csv")
+def clean_data(filename, orientation):
+    df = pandas.read_csv("./datasets/gabo/"+filename+".csv")
     res = df.loc[:,("angle_leye_lear", "angle_reye_rear", "angle_leye_nose", "angle_reye_nose", "angle_lear_nose", "angle_rear_nose")]
     res["lear"] = df["l_ear_x"] != 0
     res["rear"] = df["r_ear_x"] != 0
@@ -23,12 +17,19 @@ def clean_data(filename):
     res["d_reye_rear"] = df.loc[:,("r_eye_x", "r_eye_y", "r_ear_x", "r_ear_y", "neck_ankle_height")].apply(distance, axis=1)
     res["d_reye_nose"] = df.loc[:,("r_eye_x", "r_eye_y", "nose_x", "nose_y", "neck_ankle_height")].apply(distance, axis=1)
     res["d_nose_neck"] = df.loc[:,("nose_x", "nose_y", "neck_x", "neck_y", "neck_ankle_height")].apply(distance, axis=1)
-    res["orientation"] = filename
+    res["orientation"] = orientation
 
     res.to_csv(filename+"_res.csv")
 
-clean_data("up")
-clean_data("down")
-clean_data("left")
-clean_data("right")
-clean_data("center")
+clean_data("up3", "up")
+clean_data("down3", "down")
+clean_data("left3", "left")
+clean_data("right3_1", "right")
+clean_data("right3_2", "right")
+clean_data("center3", "center")
+
+clean_data("up4", "up")
+clean_data("down4", "down")
+clean_data("left4", "left")
+clean_data("right4", "right")
+clean_data("center4", "center")
